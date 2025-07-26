30117a4326879cfb17e39accefa59942f4004b59
anastassow <127148449+anastassow@users.noreply.github.com>
2025-07-27 00:15:03 +0300

    set the properties and the env loading it by environment variables with dotenv

--- server/src/main/java/com/anastassow/server/ServerApplication.java
+++ server/src/main/java/com/anastassow/server/ServerApplication.java
@@ -3,10 +3,22 @@ package com.anastassow.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("SPRING_APPLICATION_NAME", dotenv.get("SPRING_APPLICATION_NAME"));
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("EXPIRATIONMS", dotenv.get("EXPIRATIONMS"));
+        System.setProperty("CLIENT_ID_GOOGLE", dotenv.get("CLIENT_ID_GOOGLE"));
+
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
