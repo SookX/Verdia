ceb0181b6c5149b2fb80c3d3b65bbe9559e9d969
Vasil Vasilev <89039078+SookX@users.noreply.github.com>
2025-07-27 16:46:02 +0300

    API ready

--- ml/pipeline.py
+++ ml/pipeline.py
@@ -34,17 +34,17 @@ CHECKPOINT_DIR = str(config["logging"]["checkpoint_dir"])
 
 # Define data augmentation and preprocessing pipeline
 transforms = v2.Compose([
-    v2.ToImage(),
+#     v2.ToImage(),
     v2.RandomResizedCrop(size=224, scale=(0.8, 1.0)), 
-    v2.RandomHorizontalFlip(p=0.5), 
-    v2.RandomRotation(degrees=15),
-    v2.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-    v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+#     v2.RandomHorizontalFlip(p=0.5), 
+#     v2.RandomRotation(degrees=15),
+#     v2.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+     v2.ToDtype(torch.float32, scale=True),
+     v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 
-dataset = PlantVillage("./dataset/dist", transforms)
+dataset = PlantVillage("./dataset/dist", transforms=transforms)
 
 train_dataset, val_dataset = train_val_split(dataset)
 
@@ -52,7 +52,7 @@ train_dataloader = DataLoader(train_dataset, BATCH_SIZE, True, num_workers=0)
 val_dataloader = DataLoader(val_dataset, BATCH_SIZE, False, num_workers=0)
 print("[INFO] All data loaders have been successfully initialized.")
 
-model = Classifier(len(dataset.idx_to_class)).to(DEVICE)
+model = Classifier(len(dataset.idx_to_class), transforms).to(DEVICE)
 loss_fn = CrossEntropyLoss()
 optimizer = Adam(model.parameters(), LEARNING_RATE)
 scheduler = StepLR(optimizer, step_size=5, gamma=0.5)
